fbfb22583274c843fb68160ef2a1758969628e97
dan13g <bgheoca@gmail.com>
2026-07-26 13:37:53 +0100

    wip

--- analyses/validation/validate_client_data_vault_lineage.sql
+++ analyses/validation/validate_client_data_vault_lineage.sql
@@ -19,23 +19,4 @@ select 'BV_MASTER_CLIENT_CURRENT', count(*)
 from {{ ref('bv_master_client_current') }}
 union all
 select 'DIM_CLIENT', count(*)
-from {{ ref('dim_client') }};
-
--- Trace source identities through both hubs and the mastering link.
-select
-    master.master_client_hk,
-    source.source_client_business_key,
-    source.record_source,
-    assignment.assignment_type,
-    assignment.match_method,
-    assignment.match_rule,
-    assignment.match_score
-from {{ ref('hub_master_client') }} master
-join {{ ref('link_master_client_source_client') }} link
-    on master.master_client_hk = link.master_client_hk
-join {{ ref('hub_client') }} source
-    on link.client_hk = source.client_hk
-join {{ ref('bv_master_client_assignment') }} assignment
-    on link.master_client_hk = assignment.master_client_hk
-   and link.client_hk = assignment.client_hk
-order by master.master_client_hk, source.record_source;
+from {{ ref('dim_client') }}
